70b5b3a58e412c788fadbe5c20121263b1d62f6b
Jack Zheng <jack.zheng@stonybrook.edu>
2016-04-09 19:48:35 -0400

    Create addevent.php
    updated, missing dot

---
+++ Backend/addevent.php
@@ -0,0 +1,25 @@
+<?php 
+  
+include_once('dbinfo.php');
+$tuser = $_GET['username'];
+$tname = $_GET['eventname'];
+$tcode = $_GET['eventcode'];
+$tinfo = $_GET['eventinfo'];
+
+$db_connection = new mysqli($server_name, $db_username, $db_password, $db_name); 
+
+if ($db_connection->connect_error) {
+  echo "FALSE" . $db_connection->error;  
+} 
+
+$request = "INSERT INTO events (id, name, code, eventinfo) VALUES (" . "'" . $tuser . "'" . "," . "'" . $tname ."'" . "," . "'" . $tcode . "'" . "," . "'" . $tinfo . "'" . ")";
+
+if ($db_connection->query($request)) {
+  echo "TRUE"; 
+} else {
+  echo "FALSE";
+}
+
+mysqli_close($db_connection);
+  
+?>
