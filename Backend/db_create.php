b36780b2d9708aba565bd557c86cfe2da66ad38c
Jack Zheng <jack.zheng@stonybrook.edu>
2016-04-09 14:31:34 -0400

    Update db_create.php

--- Backend/db_create.php
+++ Backend/db_create.php
@@ -9,20 +9,45 @@ $db_password = "";			// Replace with password
 $db_conn = new mysqli($servername, $db_username, $db_password); 
 
 //	See if connection went through 
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error); 
+if ($db_conn->connect_error) {
+	die("Connection failed: " . $db_conn->connect_error); 
 }
 
 $db_table = "CREATE TABLE row1 (
-	username VARCHAR(30) PRIMARY KEY NOT NULL, 
-	password VARCHAR(30) NOT NULL,
-	events   INT(6) UNSIGNED AUTO_INCREMENT)";
+	username VARCHAR(30) 	PRIMARY KEY NOT NULL, 
+	password VARCHAR(30) 	NOT NULL,
+	events   INT(6) 	 	UNSIGNED AUTO_INCREMENT)";
 
 //	Test to see if table was made
-if ($conn->query($db_table) === TRUE) {
+if ($db_conn->query($db_table) === TRUE) {
 	echo "Table db_table was created successfully";
 } else {
-	echo "Error creating table: " . $conn->error;
+	echo "Error creating table: " . $db_conn->error;
+}
+
+$db_events_table = "CREATE TABLE row2 (
+	id 	 INT(6) 		UNSIGNED PRIMARY KEY,
+	code INT(6) 		UNSIGNED,
+	name VARCHAR(30)    NOT NULL,
+	zone VARCHAR(30)    NOT NULL)";
+
+if ($db_conn->query($db_events_table) === TRUE) {
+	echo "Table db_events_table was created successfully"; 
+} else {
+	echo "Error creating table: " . $db_conn->error;
+}
+
+$db_zone_table = "CREATE TABLE row3 (
+	id 		INT(6)			UNSIGNED PRIMARY KEY,
+	msg 	VARCHAR(30)		NOT NULL
+	lat 	DOUBLE(6,2), 
+	long 	DOUBLE(6,2),
+	radius 	DOUBLE(6,2))";
+
+if ($db_conn->query($db_zone_table) === TRUE) {
+	echo "Table db_zone_table was created successfully"; 
+} else {
+	echo "Error creating table: " . $db_conn->error;
 }
 
 
