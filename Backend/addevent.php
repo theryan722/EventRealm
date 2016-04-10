ee7666c5f34fc4f84e5476d0db717d8b517a9377
Ryan O'Day <romanguy722@gmail.com>
2016-04-09 21:14:27 -0400

    Update addevent.php

--- Backend/addevent.php
+++ Backend/addevent.php
@@ -12,14 +12,19 @@ if ($db_connection->connect_error) {
   echo "FALSE" . $db_connection->error;  
 } 
 
-$request = "INSERT INTO events (id, name, code, eventinfo) VALUES (" . "'" . $tuser . "'" . "," . "'" . $tname ."'" . "," . "'" . $tcode . "'" . "," . "'" . $tinfo . "'" . ")";
+$req = "SELECT * FROM events WHERE code = '" . $tcode . "'"; 
+
+if (mysqli_query($db_connection, $req)) {
+  echo "FALSE";
+}else{
+$request = "INSERT INTO events (id, name, code, eventinfo) VALUES (" . "'" . $tuser . "','" . $tname . "'" . "," . "'" . $tcode . "'" . "," . "'" . $tinfo . "'" . ")";
 
 if ($db_connection->query($request)) {
   echo "TRUE"; 
 } else {
   echo "FALSE";
 }
-
+}
 mysqli_close($db_connection);
   
 ?>
