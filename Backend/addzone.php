c320962cd3656f0e4a651fbb9ea17f2eec7638de
Jack Zheng <jack.zheng@stonybrook.edu>
2016-04-09 21:14:06 -0400

    Update addzone.php
    Checking zones values

--- Backend/addzone.php
+++ Backend/addzone.php
@@ -15,12 +15,20 @@ if ($db_connection->connect_error) {
   echo "FALSE" . $db_connection->error;  
 } 
 
-$request = "INSERT INTO zones (id, latitude, longitude, msg_txt, msg_email, sound) VALUES ('" . $id . "','" . $lat . "','" . $long . "','" . $msg_txt . "','" . $msg_email . "','" . $sound . "')"; 
+$request = "SELECT * FROM zones WHERE latitude = '" . $lat . "'"; 
+$request2 = "SELECT * FROM zones WHERE longitude = '" . $long . "'"; 
 
-if ($db_connection->query($request)) {
-  echo "TRUE"; 
+if (mysqli_query($db_connection, $request) && mysqli_query($db_connection, $request2)) {
+  echo "FALSE 2"; 
 } else {
-  echo "FALSE";
+  
+  $request = "INSERT INTO zones (id, latitude, longitude, msg_txt, msg_email, sound) VALUES ('" . $id . "','" . $lat . "','" . $long . "','" . $msg_txt . "','" . $msg_email . "','" . $sound . "')"; 
+
+  if ($db_connection->query($request)) {
+      echo "TRUE"; 
+    } else {
+      echo "FALSE";
+    }
 }
 
 mysqli_close($db_connection);
