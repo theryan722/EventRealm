25ad202d5230bef56e46a7c4be13d0ff02a7faf3
Jack Zheng <jack.zheng@stonybrook.edu>
2016-04-09 20:18:22 -0400

    Update checkuser.php

--- Backend/checkuser.php
+++ Backend/checkuser.php
@@ -1,28 +1,28 @@
- <?php
-  
-	include_once('dbinfo.php');
+<?php
 
-	$tuser = $_GET['username'];
-	$tpass = $_GET['password'];
-  
-	$db_connection = new mysqli($server_name, $db_username, $db_password, $db_name); 
+include_once('dbinfo.php');
 
-  if ($db_connection->connect_error) {
-    echo "FALSE" . $db_connection->error;  
-  } 
+$tuser = $_GET['username'];
+$tpass = $_GET['password'];
 
-  $query = "SELECT username from users where username='" . $tuser . "'";
-  $result = mysql_query($query);
+$db_connection = new mysqli($server_name, $db_username, $db_password, $db_name); 
 
-  if(mysql_num_rows($result) > 0)	{
-    $check_password = "SELECT password from users where username='" . $tuser . "'";
-    if ($db_connection->query($check_password)) {
-      echo "TRUE"; 
-    } else {
-      echo "FALSE";
-    }
-  } else{
-   		echo "FALSE"; 
+if ($db_connection->connect_error) {
+  echo "FALSE" . $db_connection->error;  
+} 
+
+$query = "SELECT username from users where username='" . $tuser . "'";
+$result = mysqli_query($db_connection, $query);
+
+if($db_connection->query($result))	{
+  $check_password = "SELECT password from users where username='" . $tuser . "'";
+  if ($db_connection->query($check_password)) {
+    echo "TRUE"; 
+  } else {
+    echo "FALSE";
   }
-  
-  ?>
+} else{
+    echo "FALSE"; 
+}
+
+?>
