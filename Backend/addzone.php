c0be8b76f9dc9e244bd7fd44dee6ef2ef456d353
Ryan O'Day <romanguy722@gmail.com>
2016-04-09 21:33:22 -0400

    Fix addzone

--- Backend/addzone.php
+++ Backend/addzone.php
@@ -4,25 +4,24 @@ include_once('dbinfo.php');
 
 $db_connection = new mysqli($server_name, $db_username, $db_password, $db_name); 
 
-$id = $_GET['event']; 
+$id = $_GET['event'];
+$name = $_GET['name'];
+$radius = $_GET['radius'];
 $lat = $_GET['latitude']; 
 $long = $_GET['longitude']; 
 $msg_txt = $_GET['msg_txt']; 
 $msg_email = $_GET['msg_email']; 
-$sound = $_GET['sound']; 
+$sound = $_GET['sound'];
 
 if ($db_connection->connect_error) {
   echo "FALSE" . $db_connection->error;  
 } 
 
-$request = "SELECT * FROM zones WHERE latitude = '" . $lat . "'"; 
-$request2 = "SELECT * FROM zones WHERE longitude = '" . $long . "'"; 
-
-if (mysqli_query($db_connection, $request) && mysqli_query($db_connection, $request2)) {
-  echo "FALSE 2"; 
-} else {
-  
-  $request = "INSERT INTO zones (id, latitude, longitude, msg_txt, msg_email, sound) VALUES ('" . $id . "','" . $lat . "','" . $long . "','" . $msg_txt . "','" . $msg_email . "','" . $sound . "')"; 
+$req = "SELECT * FROM zones WHERE name = '" . $name . "'"; 
+if (mysqli_query($db_connection, $req)){
+ echo "FALSE";
+}else{
+$request = "INSERT INTO zones (id, name, radius, latitude, longitude, msg_txt, msg_email, sound) VALUES ('" . $id . "','" . $name . "','" . $radius . "','" . $lat . "','" . $long . "','" . $msg_txt . "','" . $msg_email . "','" . $sound . "')"; 
 
   if ($db_connection->query($request)) {
       echo "TRUE"; 
@@ -30,6 +29,7 @@ if (mysqli_query($db_connection, $request) && mysqli_query($db_connection, $requ
       echo "FALSE";
     }
 }
+  
 
 mysqli_close($db_connection);
 ?>
